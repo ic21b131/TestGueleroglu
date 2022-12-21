03a7f585c5e6999f78e9be1f7d037afd4ca367c4
ic21b131 <ic21b131@technikum-wien.at>
2022-12-21 18:08:26 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/testgueleroglu/TestGuelerogluApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.testgueleroglu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGuelerogluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGuelerogluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtw/testgueleroglu/TestGuelerogluApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.testgueleroglu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGuelerogluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
