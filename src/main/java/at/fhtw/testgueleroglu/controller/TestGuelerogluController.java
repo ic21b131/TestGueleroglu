6dc95a15d8b531fc73c9e7e01bdbcb59953e35ad
ic21b131 <ic21b131@technikum-wien.at>
2022-12-21 19:07:58 +0100

    Newer function is implemented

--- src/main/java/at/fhtw/testgueleroglu/controller/TestGuelerogluController.java
+++ src/main/java/at/fhtw/testgueleroglu/controller/TestGuelerogluController.java
@@ -10,8 +10,21 @@ public class TestGuelerogluController {
     @GetMapping("/restapi")
     public String GetGrade(@RequestParam int percentageValue) {
 
-        if (percentageValue >= 80) {
+        if (percentageValue >= 88) {
             return "1";
+
+             /*           if ((percentageValue >= 75) && percentageValue < 88)) {
+            return "2";
+        }
+
+            if ((percentageValue >= 63) && percentageValue < 75))
+            return "3";
+
+            if ((percentageValue >= 50) && percentageValue < 63))           nicht fertig geworden, wegen der Zeit
+            return "4";
+
+            if (percentageValue < 50)
+            return "5"; */
 
         } else
             return "ausnahme";
